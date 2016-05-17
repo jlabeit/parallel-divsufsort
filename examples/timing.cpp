052c711027b32ae95be2d76b25e7f4804ab72626
jlabeit <julianlabeit@gmail.com>
2016-05-17 15:11:44 +0200

    Added memory usage and decimal formatting to timing.

--- examples/timing.cpp
+++ examples/timing.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <string>
 
+#include <unistd.h>
+#include <sys/resource.h>
+#include <stdio.h>
+
 #include <divsufsort.h>
 
 
@@ -11,13 +15,19 @@ using namespace std;
 typedef int32_t num_type; // Currently only int32_t and int64_t are supported.
 int times = 3; // How often the time measurement is repeated.
 
+size_t getPeakRSS() {
+	struct rusage rusage;
+	getrusage( RUSAGE_SELF, &rusage);
+	return (size_t)(rusage.ru_maxrss * 1024L);
+}
+
 int main(int argc, char* args[]) {
 	if (argc != 2) {
 		cout << "Expected one argument (input file)."
 			<< endl;	
 		return -1;
 	}
-
+	std::cout.precision(4);
 	string text;
 	{ // Read input file.
 		ifstream input_file(args[1]);
@@ -27,7 +37,6 @@ int main(int argc, char* args[]) {
 		text.assign((istreambuf_iterator<char>(input_file)),
 				istreambuf_iterator<char>());
 	}
-	//text += '\0';
 	num_type size = text.size();
 	num_type *SA = new num_type[size];
 	for (int i = 0; i < times; ++i) {
@@ -35,11 +44,10 @@ int main(int argc, char* args[]) {
 		divsufsort((sauchar_t*)text.data(), SA, size);
 		auto end = chrono::steady_clock::now();
 		auto diff = end - start;
-		cout <<	chrono::duration <double, milli> (diff).count();
+		cout <<	chrono::duration <double, milli> (diff).count() / 1000.0 << ", ";
 		cout.flush();
-		if (i < times -1) cout << ", ";
 	}
-	cout << endl;
+	cout << getPeakRSS() / (1024*1024)<< endl;
 	if (sufcheck((sauchar_t*)text.data(), SA, size, false)) {
 		cout << "Sufcheck failed!" << endl;
 		return -1;
